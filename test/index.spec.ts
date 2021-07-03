3a7e71353b135b5c30339e75a1ebe4f58599db6a
starfrogsplash <ali_khalil@hotmail.co.uk>
2021-07-03 16:40:08 +0100

    set up app with basic get endpoints

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,12 @@
 import request from "supertest";
+import {app}from '../src/app'
 
-import {app}from '../app'
-
-it('checks endpoint', async()=> {
+it('checks default endpoint', async()=> {
     const res = await request(app).get('/')
-    expect(res.body).toEqual('we have a success')
+    expect(res.body).toEqual('success')
+})
+
+it('returns id', async()=> {
+    const res = await request(app).get('/abc1')
+    expect(res.body).toEqual('id is abc1')
 })
